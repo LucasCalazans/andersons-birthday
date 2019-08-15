2578eec5ec82f3299a7158fa2aca3e9356121d9d
Lucas Calazans <calazans95@hotmail.com>
2019-08-15 14:07:20 -0300

    onSuccess

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 import Countdown from 'react-countdown-now';
+import Finished from './Finished';
 
 const THE_MOST_AWAITED_DAY = new Date(2019, 7, 17);
 
-const App = () => (
-    <div className="app">
-        <h1>Tempo até o aniversário do ANO:</h1>
-        <Countdown date={THE_MOST_AWAITED_DAY} />
-    </div>
-);
+const App = () => {
+    let countDownRef = useRef(null);
+    const [finished, setFinished] = useState(false);
+
+    const isCompleted = () => {
+        const countdown = countDownRef.current;
+        if (!countdown) return false;
+
+        return countdown.isCompleted();
+    };
+
+    useEffect(() => {
+        if (!['#parab%C3%A9ns', '#parabens'].includes(document.location.hash) || ['#parab%C3%A9ns=1', '#parabens=1'].includes(document.location.hash)) {
+            setTimeout(() => setFinished(isCompleted()), 1500);
+        }
+    }, []);
+
+    return finished ? <Finished /> : (
+        <div className="app">
+            <div className="app-wrapper">
+                <h1>Tempo até o aniversário do ANO:</h1>
+                <Countdown ref={countDownRef} date={THE_MOST_AWAITED_DAY} onComplete={() => console.log('opa') || setFinished(true)} />
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Finished.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Finished = () => {
+    return (
+        <div>
+            <img src="./firework.webp" alt="" className="firework"/>
+            <h1>Parabéns pelas suas 2 (DUAS) certificações <strong>@Anderson Oliveira</strong></h1>
+            <img src="./magento-certification-profile.png" alt="" className="certification-image"/>
+        </div>
+    );
+};
+
+export default Finished;
